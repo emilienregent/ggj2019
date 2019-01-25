d3ab74a11ebc4d725ac7eeb7a9183221e3337340
PaperMartin <martin.wetischek@orange.fr>
2019-01-25 23:36:17 +0100

    Interactable With Event
    Interactable component qui éxécute un UnityEvent customisable dans Interact()

---
+++ Assets/Sources/InteractableWithEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableWithEvent : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private UnityEvent OnInteract;
+
+    public void Interact()
+    {
+        OnInteract.Invoke();
+    }
+}
