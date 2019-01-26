618b7f3ea2812ca9cb0e40d9bb706e1920ea2fdd
PaperMartin <martin.wetischek@orange.fr>
2019-01-26 09:37:24 +0100

    Fixed forgotten Interface

--- Assets/Sources/HumanMotor.cs
+++ Assets/Sources/HumanMotor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
-public class HumanMotor : MonoBehaviour
+public class HumanMotor : MonoBehaviour, ICharacter
 {
     private Rigidbody2D rbody;
     [SerializeField]
