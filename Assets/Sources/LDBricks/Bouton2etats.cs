360732f2ac8a8211e71bb908b143076bef64eb69
PaperMartin <martin.wetischek@orange.fr>
2019-01-26 11:05:11 +0100

    Bouton Prefab
    Change d'état après interaction
    true = porte ouverte
    false = porte fermée

---
+++ Assets/Sources/LDBricks/Bouton2etats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Bouton2etats : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private UnityEvent OnButtonOn;
+    [SerializeField]
+    private UnityEvent OnButtonOff;
+    private bool ButtonOn = false;
+
+    public void Interact()
+    {
+        if (ButtonOn)
+        {
+            OnButtonOff.Invoke();
+        }
+        else OnButtonOn.Invoke();
+        ButtonOn = !ButtonOn;
+    }
+
+    public void DashIn()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<ICharacter>() != null)
+            collision.GetComponent<ICharacter>().InteractableList.Add(this);
+    }
+    private void OnTriggerExit(Collider2D collision)
+    {
+        if (collision.GetComponent<ICharacter>() != null)
+            collision.GetComponent<ICharacter>().InteractableList.Remove(this);
+    }
+}
