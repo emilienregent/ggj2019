1104fef5eaffabb1eabb1139e584b0c5902c1250
Guillaume Olivetti <olivetti.guillaume@gmail.com>
2019-01-26 11:26:27 +0100

    Game camera script and anchors

---
+++ Assets/Sources/Camera/CameraAnchor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraAnchor : MonoBehaviour
+{
+    public int index = 1;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, .5f);
+    }
+}
