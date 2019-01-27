c0ae1c39be8cf49bd3d2c73b96e77d8ef7255a91
PaperMartin <martin.wetischek@orange.fr>
2019-01-27 13:48:03 +0100

    stinger

---
+++ Assets/Stinger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stinger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<AudioSource>().Play();
+        gameObject.SetActive(false);
+    }
+}
